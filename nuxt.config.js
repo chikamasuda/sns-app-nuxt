58b6db15df33462626f17a0fe4589a3844151f61
chikamasuda <c.m.062820@gmail.com>
2022-05-19 23:01:11 +0900

    proxyの設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,6 +66,14 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: development ? 'http://localhost:8000' : 'https://thawing-sea-59706.herokuapp.com/',
+    proxy: true,
+  },
+
+  proxy: {
+    '/api/': {
+      target: 'https://thawing-sea-59706.herokuapp.com/',
+      pathRewrite: {'^/api/': '/'}
+    },
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
