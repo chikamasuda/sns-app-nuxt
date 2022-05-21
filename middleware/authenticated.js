d5c3a565203db9ec932fe6977333f8cd342a5189
chikamasuda <c.m.062820@gmail.com>
2022-05-21 13:53:47 +0900

    middlewareでログインチェックの実装完了

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,9 +1,12 @@
 import firebase from '~/plugins/firebase'
 
-export default function ({ route, store, redirect }) {
-  firebase.auth().onAuthStateChanged(user  => {
-    if (!user) {
-      if(route.name !== "login") redirect("/login")
-    } 
+export default async function ({ redirect }) {
+  await new Promise((resolve) => {
+    firebase.auth().onAuthStateChanged(user => {
+      if (!user) {
+        return redirect('/login')
+      }
+      resolve()
+    })
   })
 }
