c7cc536d24ecbf4e129f19e4dbc16dc32c29622e
chikamasuda <c.m.062820@gmail.com>
2022-05-23 07:08:36 +0900

    ロゴのCSS変更

--- layouts/header.vue
+++ layouts/header.vue
@@ -2,7 +2,7 @@
   <nav class="container">
     <ul class="header">
       <li>
-        <h1 class="header-logo"><a href="#"><img src="/img/logo.png" alt="ロゴ" width="150"></a></h1>
+        <h1 class="header-logo"><a href="#"><img src="/img/logo.png" alt="ロゴ"></a></h1>
       </li>
       <li class="header-menu">
         <NuxtLink to="/register" class="header-menu-item">新規登録</NuxtLink>
